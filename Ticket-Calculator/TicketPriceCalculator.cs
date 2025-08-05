35cba2e7981d7d1aa527ad05219dfae4d5fb7798
Emmanuel Mensah <ekmensah065@st.ug.edu.gh>
2025-08-05 11:54:46 +0000

    Added Ticket Price Calculator

---
+++ Ticket-Calculator/TicketPriceCalculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class TicketPriceCalculator
+{
+    static void Main()
+    {
+        Console.Write("Enter your age: ");
+        int age = int.Parse(Console.ReadLine());
+
+        int basePrice = 10;
+        int discount = 3;
+        int ticketPrice;
+
+        if (age <= 12 || age >= 65)
+            ticketPrice = basePrice - discount;
+        else
+            ticketPrice = basePrice;
+
+        Console.WriteLine($"Your ticket price is: GHC{ticketPrice}");
+    }
+}
