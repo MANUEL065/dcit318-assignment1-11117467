2e53b2133138da5b1717861f395bf100a1fed472
Emmanuel Mensah <ekmensah065@st.ug.edu.gh>
2025-08-05 12:11:46 +0000

    Added Triangle Type Identifier

---
+++ Triangle-Type-Identifier/TriangleTypeIdentifier.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class TriangleTypeIdentifier
+{
+    static void Main()
+    {
+        Console.Write("Enter length of side A: ");
+        double a = double.Parse(Console.ReadLine());
+
+        Console.Write("Enter length of side B: ");
+        double b = double.Parse(Console.ReadLine());
+
+        Console.Write("Enter length of side C: ");
+        double c = double.Parse(Console.ReadLine());
+
+        if (a == b && b == c)
+        {
+            Console.WriteLine("Triangle Type: Equilateral");
+        }
+        else if (a == b || b == c || a == c)
+        {
+            Console.WriteLine("Triangle Type: Isosceles");
+        }
+        else
+        {
+            Console.WriteLine("Triangle Type: Scalene");
+        }
+    }
+}
